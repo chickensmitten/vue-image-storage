55deb0303d0b151c23b987a62b0380a3538b7128
chickensmitten <chia.shengyeong@gmail.com>
2020-05-16 21:30:40 +0800

    Added images to image vue

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
-    <h1>Image List</h1>
+    <img 
+    v-for="image in allImages" 
+    :src="image.link" 
+    :key="image.id" />
   </div>  
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'ImageList',
+  computed: mapGetters(['allImages']),
   methods: mapActions(['fetchImages']),
   created() {
     this.fetchImages();
